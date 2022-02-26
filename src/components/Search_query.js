c6098e8285c7f005d415561abc0aa2a72b77d09e
Tanush Goel <tanushgoel03@gmail.com>
2022-02-26 00:56:25 -0800

    adding queries

--- src/components/Search_query.js
+++ src/components/Search_query.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Fuse from 'fuse.js';
 import "./Search_query.css";
 
-const games_list=["counter strike:global offensive"];
+const games_list=["counter strike:global offensive","CS"];
 const options={};
 const fuse= new Fuse(games_list,options)
 
@@ -15,45 +15,48 @@ function SearchGame(props){
         setSearch(event.target.value)
     }
     const handleSubmit=(event)=>{
+        //SsetGamelist([])
         event.preventDefault()
         const similarGames= fuse.search(games,options)
-        console.log(similarGames)
+        //console.log(similarGames)
         if (similarGames.length!=0){
+            //console.log(similarGames)
             setGamelist(similarGames)
-            
         }else{ 
             setGamelist([{item:"No game found"}])
         }
-        setSearch('')
         console.log(possibleGames)
-
+        setSearch('')
     }
 
     const handleAdd=(event)=>{
         event.preventDefault()
+        {console.log(event.target.value)}
         if (!userGames.includes(event.target.value)){
-            setGames(event.target.value)
+            setGames(userGames.concat(event.target.value))
         }
+        {console.log(userGames)}
     }
 
-    return <div>
+    return <div className="dropdown">
         <h1>Search for games</h1>
         <form onSubmit={handleSubmit}>
             <label>Enter favorite Games</label>
-            <div className="dropdown">
-                <input type='text' value={games}  placeholder ='Search..' onChange={handleGame}></input>
-                <button type='submit'>Search Game</button> 
-            </div>
-            
+            <input type='text' value={games}  placeholder ='Search..' onChange={handleGame}></input>
+            <button type='submit'>Search Game</button> 
         </form>
         <div className="dropdown-content">
-                {possibleGames.map((usGame)=>
-                <button key= {usGame} value={usGame} onClick={handleAdd}>{usGame.item}</button>
-            )}   
+            {possibleGames.map((posGame)=>
+                {if(posGame.item!="No game found"){
+                    return <button key= {posGame.item} value={posGame.item} onClick={handleAdd}>{posGame.item}</button>
+                }else{
+                     return <p key={posGame.item}>No game Found</p>
+                }})}   
         </div>
         <div className='games'>
+            {console.log(userGames)}
             {userGames.map((usGame)=>
-                <p key={usGame}>{usGame.item}</p>
+                <p key={usGame}>{usGame}</p>
             )}
         </div>
 
