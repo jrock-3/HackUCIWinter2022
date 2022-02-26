b5aada252fdebe38cbc1ca7d35b5189b7606cddf
jrock <justin.liao3@gmail.com>
2022-02-25 23:59:46 -0800

    fixed GameCard prop names and added rating percentages, added searchbar to App.js

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,6 +1,10 @@
 import './GameCard.css'
 
 function GameCard(props) {
+  const x = props.pos_reviews / (props.pos_reviews + props.neg_reviews) * 100
+  const percent_pos = Math.round(x * 100) / 100;
+  const percent_neg = Math.round((100 - percent_pos) * 100) / 100
+  console.log(percent_neg + " " + percent_pos)
   return (
     <div id="game-card">
       <div id="game-name">{props.title}</div>
@@ -12,8 +16,8 @@ function GameCard(props) {
       </div> */}
       <div id="rating-box">
         <p>Ratings:</p>
-        <div id="rating-box1" style={{width: props.percent_pos + "%"}}>{props.percent_pos+"%"}</div>
-        <div id="rating-box2" style={{width: 100-props.percent_pos + '%'}}>{props.percent_pos+"%"}</div>
+        <div id="rating-box1" style={{width: percent_pos + "%"}}>{percent_pos+"%"}</div>
+        <div id="rating-box2" style={{width: percent_neg + '%'}}>{percent_neg+"%"}</div>
       </div>
       <a href={props.news_link}><p id="game-news-link">News</p></a>
     </div>
