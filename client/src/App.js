d52a0d3fd371a48b3a080a54e9c034d35a6c2d44
jrock <justin.liao3@gmail.com>
2022-02-26 11:51:59 -0800

    started connecting FetchInfo to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import GameCard from './components/GameCard.js';
 import SearchGame from './components/Search_query.js';
-import Home from './Home.js';
+import FetchInfo from './FetchInfo.js';
 
 
 function App() {
 
-  const gamecard_props = [{
+  const fetch_props = {
+    id: 440
+  }
+  
+  const gamecard_props = [
+  {
     title: "Team Fortress 2",
     img: "https://cdn.akamai.steamstatic.com/steam/apps/440/header.jpg?t=1592263852",
     dev_name: "Valve",
@@ -15,11 +20,14 @@ function App() {
     neg_reviews: 836,
     news_link: "https://steamstore-a.akamaihd.net/news/externalpost/steam_community_announcements/4235075565596422445",
   },
+  FetchInfo(fetch_props),
   ];
+
+  console.log(gamecard_props)
+
+  // const x = await FetchInfo({id:440});
   return (
     <div>
-      <Home />
-      
       <div>
         <SearchGame />
       </div>
