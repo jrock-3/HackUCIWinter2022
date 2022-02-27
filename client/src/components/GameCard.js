df0d0b1a8bdfe9485a0bda623d285f8187cb0721
jrock <justin.liao3@gmail.com>
2022-02-26 21:04:13 -0800

    added mail

--- client/src/components/GameCard.js
+++ client/src/components/GameCard.js
@@ -1,22 +1,20 @@
 import './GameCard.css'
 import deleteIcon from '../images/deleteIcon.png'
+import newsIcon from '../images/newsIcon.png'
 
 function GameCard(props) {
   const x = props.pos_reviews / (props.pos_reviews + props.neg_reviews) * 100
   const percent_pos = Math.round(x * 100) / 100;
   const percent_neg = Math.round((100 - percent_pos) * 100) / 100
-  // console.log(percent_neg + " " + percent_pos)
   return (
     <div className="game-card">
       <img src={props.img} className="game-img" alt={"Image of " + props.title} />
-      <img src = {deleteIcon} id="delete-button" onClick={props.onClick} className={props.title}/>
+      <img src = {deleteIcon} id="delete-button" onClick={props.onClick} className={props.title} alt="Delete Button" />
+      <a href={props.news_link} target="_blank"><img src={newsIcon} id="news-image" alt="News Button" /></a>
       <div className="bottom-card">
         <div className="game-name">{props.title}</div>
         <div className="dev-name">Developer: {props.dev_name}</div>
         <div className="game-player-count">Online Players: {props.total_players} </div>
-        {/* <div id="game-ratings">
-          put ratings here
-        </div> */}
         <div className="rating-box">
           <div className="rating-box1" style={{width: percent_pos + "%"}}></div>
           <div className="rating-box2" style={{width: percent_neg + '%'}}></div>
@@ -26,7 +24,6 @@ function GameCard(props) {
           <p>Negative Ratings:<br />{percent_neg+"%"}</p>
         </div>
       </div>
-      {/* <a href={props.news_link}><p className="game-news-link">News</p></a> */}
     </div>
   );
 }
